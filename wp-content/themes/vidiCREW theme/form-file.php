92e2ed00660dd943c3cb707c2bedc36acb6c334b
root <hitaxi.tt@gmail.com>
2018-05-05 16:21:03 +0530

    Files for app api

---
+++ wp-content/themes/vidiCREW theme/form-file.php
@@ -0,0 +1,137 @@
+<?php
+global $wpdb;
+$userArr = $wpdb->get_results("SELECT notification,type,status,DATE_FORMAT(created_date, %d-%m-%Y) as created_date FROM wp_notification where status='1'");
+$args = array('meta_query' => array(
+        array('key' => 'device_id', 'value' => '', 'compare' => '!='),
+        array('key' => 'device_type', 'value' => '', 'compare' => '!='),
+        ));
+$userArr = get_users($args);
+
+?>
+<style>
+    /*.row{ display: table;
+    
+    content: " "; }
+    .clearfix{ clear: both;}*/
+    .form-horizontal .form-group {
+
+        margin-right: -15px;
+        margin-left: -15px;
+
+    }
+    .form-group {
+
+        margin-bottom: 15px;
+
+    }
+    label {
+
+        font-weight: 600;
+
+    }
+    label {
+
+        display: inline-block;
+        max-width: 100%;
+
+    }
+    .form-control {
+
+        height: 36px;
+        padding: 3px 10px;
+        border: 1px solid #dddddd;
+        border-radius: 3px;
+        color: #555;
+
+    }
+
+    .form-control {
+
+        display: block;
+        width: 100%;
+        height: 34px;
+        padding: 6px 12px;
+        font-size: 14px;
+        line-height: 1.42857143;
+        color: #555;
+        background-color: #fff;
+        background-image: none;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+
+    }
+
+</style>
+<h1>Broadcast Notification</h1>
+<div>
+
+    <?php if ($successMessage != '') { ?> <div><span style="color:green;font-weight: bold;"><?php echo $successMessage; ?></span></div> <?php
+    }
+    if ($errorMessage != '') {
+        ?> <div><span style="color:red;font-weight: bold;"><?php echo $errorMessage; ?></span></div>  <?php
+        }
+        ?>
+
+
+    <form method="POST">
+        <div class="row cleafix">
+            <?php
+            if (isset($userArr) && count($userArr) > 0) {
+                ?>
+                <div class="form-group">
+                    <label class="control-label">Notification</label>
+                    <div class="">
+                        <textarea class="" name="notification" rows="4" cols="80"></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class=" control-label">Notification Type</label>
+                    <div class="">
+                        <input class="form-control" name="type" value="user_specific" type="radio">User Specific
+                        <input class="form-control" name="type" value="broadcast" type="radio" checked="checked">Broadcast
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class=" control-label">Select User</label>
+                    <div class="">
+                        <select name="device_user[]" id="device_user" multiple="">
+                            <?php
+                            if (isset($userArr) && count($userArr) > 0) {
+                                foreach ($userArr as $data) {
+                                    $wpdb->insert(
+                                            'wp_notification',
+                                            array(
+                                        'user_id' => $data['id'],
+                                        'notification' => $msg,
+                                        'created_date' => date('Y-m-d H:i:s'),
+                                        'status' => '1',
+                                        'type' => $type
+                                            )
+                                    );
+                                    ?><option value="<?php echo $data['id']; ?>"><?php echo $data['display_name']; ?></option><?php
+                                }
+                            }
+                            ?>
+                        </select>
+
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class=" control-label"></label>
+                    <div class="">
+                        <input class="btn btn-primary" name="btnSubmit" value="Send notification" type="Submit">
+
+                    </div>
+                </div>
+                <?php
+            }else{
+                
+                echo '<b>No record found.</b>';
+            }
+            ?>
+
+        </div>
+    </form>
+</div>
--- wp-content/themes/vidiCREW theme/functions.php
+++ wp-content/themes/vidiCREW theme/functions.php
@@ -143,3 +143,252 @@ function my_text_strings( $translated_text, $text, $domain ) {
 }
 add_filter( 'gettext', 'my_text_strings', 20, 3 );
 */
+/** notification start */
+add_action("admin_menu", "add_notification_menu_item");
+
+function add_notification_menu_item() {
+    add_menu_page("Notifications", "Notifications", "manage_options",
+            "notification", "notification");
+}
+
+function notification() {
+    global $wpdb;
+    $successMessage = '';
+    $errorMessage = '';
+    if (isset($_POST['notification'])) {
+
+        if ($_POST['notification'] != '') {
+            $msg = $_POST['notification'];
+            $type = isset($_POST['type']) ? $_POST['type'] : 'broadcast';
+            if ($type == 'broadcast') {
+                $userArr = $wpdb->get_results("SELECT device_id, device_type, display_name,id FROM wp_users where device_id!= '' and device_type != '' ");
+
+                if (isset($userArr) && count($userArr) > 0) {
+                    foreach ($userArr as $data) {
+                        $wpdb->insert(
+                                'wp_notification',
+                                array(
+                            'user_id' => $data->id,
+                            'notification' => $msg,
+                            'created_date' => date('Y-m-d H:i:s'),
+                            'status' => '1',
+                            'type' => $type)
+                        );
+                        $ipquery = $wpdb->get_results("SELECT notification,type,status,DATE_FORMAT(created_date, %d-%m-%Y) as created_date FROM wp_notification where status='1' and user_id=" . $data->id);
+                        $count = $ipquery->num_rows;
+                        //$count = '1';
+                        $message = array
+                            (
+                            'message' => $msg,
+                            'title' => '',
+                            'vibrate' => 1,
+                            'sound' => 1,
+                            'notification_count' => $count
+                        );
+                        $msg1 = sendPushNotification(array($data->device_id),
+                                $message, $data->device_type);
+                    }
+                }
+            } else if ($type == 'user_specific' && isset($_POST['device_user'])) {
+
+                if (count($_POST['device_user']) > 0) {
+                    foreach ($_POST['device_user'] as $key => $val) {
+                        $data = $wpdb->get_row("SELECT device_id, device_type, display_name,id FROM wp_users where device_id!= '' and device_type != '' and id=" . $val);
+
+                        if (isset($data->id)) {
+                            $wpdb->insert(
+                                    'wp_notification',
+                                    array(
+                                'user_id' => $val,
+                                'notification' => $msg,
+                                'created_date' => date('Y-m-d H:i:s'),
+                                'status' => '1',
+                                'type' => $type
+                                    )
+                            );
+                            $ipquery = $wpdb->get_results("SELECT notification,type,status,DATE_FORMAT(created_date, %d-%m-%Y) as created_date FROM wp_notification where status='1' and user_id=" . $val);
+                            $count = $ipquery->num_rows;
+                            $message = array
+                                (
+                                'message' => $msg,
+                                'title' => '',
+                                'vibrate' => 1,
+                                'sound' => 1,
+                                'notification_count' => $count
+                            );
+                            $msg1 = sendPushNotification(array($data->device_id),
+                                    $message, $data->device_type);
+                        }
+                    }
+                }
+            }
+            $successMessage = 'Notification sent successfully.';
+        } else {
+            $errorMessage = 'Please enter notification.';
+        }
+    }
+    include 'form-file.php';
+}
+
+//function to send push notification
+function sendPushNotification($registrationIds = array(), $message = array(),
+        $deviceType) {
+    $deviceType = strtolower($deviceType);
+    if ($deviceType == 'android') {
+        $url = 'https://android.googleapis.com/gcm/send';
+        $fields = array(
+            'registration_ids' => $registrationIds,
+            'data' => $message,
+        );
+
+// Update your Google Cloud Messaging API Key
+        if (!defined('GOOGLE_API_KEY')) {
+            define("GOOGLE_API_KEY",
+                    "AAAAXcVCasQ:APA91bHjxKcFVvNgLDak_A5FuLWpd_WTy4VTozFkDkdyk5bnScL2iPYNpdaCEExFuUTxSClpu-hndvL-a3jgWI4PL1-Y8rDRxVIgNX29MN7HejK_bZlB9x0_uNGisMFQH15_4lCSnDal");
+        }
+        $headers = array(
+            'Authorization: key=' . GOOGLE_API_KEY,
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $res = json_decode($result);
+
+        return TRUE;
+    } else if (strtolower($deviceType) == 'ios') {
+
+        $deviceToken = $registrationIds[0];
+        $passphrase = '';
+
+
+        $fileName = 'Certificates.pem';
+//            $gateway = 'ssl://gateway.push.apple.com:2195';
+        $gateway = 'ssl://gateway.sandbox.push.apple.com:2195';
+
+
+        $ctx = stream_context_create();
+        // ck.pem is your certificate file
+        stream_context_set_option($ctx, 'ssl', 'local_cert',
+                get_template_directory() . '/' . $fileName);
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+        // Open a connection to the APNS server
+        $fp = stream_socket_client($gateway, $err, $errstr, 60,
+                STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+
+        if (!$fp)
+            exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+        $msg = $message['message'];
+
+        // Create the payload body
+        $body['aps'] = array(
+            'alert' => array(
+                'title' => $message['title'],
+                'body' => $msg,
+            ),
+            'badge' => (int) $message['notification_count'],
+            'sound' => 'default'
+        );
+
+        // Encode the payload as JSON
+        $payload = json_encode($body);
+
+        // Build the binary notification
+        $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n',
+                        strlen($payload)) . $payload;
+        // Send it to the server
+        $result = fwrite($fp, $msg, strlen($msg));
+
+        // Close the connection to the server
+        fclose($fp);
+
+        if (!$result)
+            return 'Message not delivered' . PHP_EOL;
+        else
+            return 'Message successfully delivered' . PHP_EOL;
+    }
+}
+
+/** notification end */
+/** add custom post type start */
+add_action('init', 'fun_custom_post_types');
+
+function fun_custom_post_types() {
+    $allPostTypes = array(
+        // Events Post Type
+        array(
+            'post_type' => 'shotlist',
+            'singular' => 'Shotlist',
+            'plural' => 'Shotlist',
+            'genname' => 'shotlist',
+            'menuicon' => 'dashicons-flag',
+            'supportarr' => array('title')
+        ),
+        
+    );
+    foreach ($allPostTypes as $postType) {
+        $post_type = $postType['post_type'];
+        $singular = $postType['singular'];
+        $plural = $postType['plural'];
+        $genrlname = $postType['genname'];
+        $menuicon = $postType['menuicon'];
+        $supportarr = $postType['supportarr'];
+
+        $labels = array(
+            'name' => _x($plural . 's', $genrlname . 's', 'vidicrew'),
+            'singular_name' => _x($singular, $genrlname, 'vidicrew'),
+            'menu_name' => _x($plural . 's', $genrlname . 's', 'vidicrew'),
+            'name_admin_bar' => _x($singular, $genrlname, 'vidicrew'),
+            'add_new' => _x('Add New', $singular, 'vidicrew'),
+            'add_new_item' => __('Add New ' . $singular, 'vidicrew'),
+            'new_item' => __('New ' . $singular, 'vidicrew'),
+            'edit_item' => __('Edit ' . $singular, 'vidicrew'),
+            'view_item' => __('View ' . $singular, 'vidicrew'),
+            'all_items' => __('All ' . $plural . 's', 'vidicrew'),
+            'search_items' => __('Search ' . $plural, 'vidicrew'),
+            'parent_item_colon' => __('Parent ' . $plural . ':', 'vidicrew'),
+            'not_found' => __('No ' . $singular . ' found.', 'vidicrew'),
+            'not_found_in_trash' => __('No ' . $singular . ' found in Trash.',
+                    'vidicrew')
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'query_var' => true,
+            'rewrite' => true,
+            'capability_type' => 'post',
+            'capabilities' => array(
+                'publish_posts' => 'manage_options',
+                'edit_posts' => 'manage_options',
+                'edit_others_posts' => 'manage_options',
+                'delete_posts' => 'manage_options',
+                'delete_others_posts' => 'manage_options',
+                'read_private_posts' => 'manage_options',
+                'edit_post' => 'manage_options',
+                'delete_post' => 'manage_options',
+                'read_post' => 'manage_options',
+            ),
+            'has_archive' => true,
+            'hierarchical' => false,
+            'menu_position' => 5,
+            'menu_icon' => $menuicon,
+            'supports' => $supportarr
+        );
+        register_post_type($post_type, $args);
+    }
+}
+/** add custom post type end */
